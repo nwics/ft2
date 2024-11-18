491cc855ba1b4ffafe24579a88a19382cb987a0a
nabilwics <nabilwicaksono274@gmail.com>
2024-11-18 07:24:09 +0700

    first commit

---
+++ src/main/java/com/example/ft2/Ft2Application.java
@@ -0,0 +1,13 @@
+package com.example.ft2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ft2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ft2Application.class, args);
+	}
+
+}
